08b24bd9c0b493d8545eba561ad86d20f32ff2aa
Tamiya <tamiyasigmar@users.noreply.github.com>
2026-05-05 21:25:04 +0200

    #1243 REFACTOR Drop kqueue scheduler source

--- code/src/abb/abb_kqueue.mk
+++
@@ -1,9 +0,0 @@
-include $(SRC_DIR)/abb.mk
-
-$(SRC_DIR)/abb.ml: .selector-kqueue
-	mkdir -p "$(SRC_DIR)"
-	echo "include Abb_scheduler_kqueue" > "$(SRC_DIR)/abb.ml"
-
-.selector-kqueue:
-	rm .select-* || true
-	touch .selector-kqueue
--- code/tests/abb_scheduler_kqueue/test.ml
+++
@@ -1,3 +0,0 @@
-module Test = Abb_test.Make (Abb_scheduler_kqueue)
-
-let () = Test.run_tests ()
